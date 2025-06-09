20d78a13f50bbbab9d8c97960479e94b80e8c958
marian.szabo@rabobank.nl <marian.szabo@rabobank.nl>
2025-06-09 11:55:40 +0200

    Spring boot dummy API generated using spring initializr https://start.spring.io/

---
+++ src/main/java/nl/ing/mortgage/affordability/MortgageAffordabilityApplication.java
@@ -0,0 +1,13 @@
+package nl.ing.mortgage.affordability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageAffordabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageAffordabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/ing/mortgage/affordability/MortgageAffordabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ing.mortgage.affordability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageAffordabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
