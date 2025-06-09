bbdd200f3187e3e3613ccf3083c017bdecaff04f
marian.szabo@rabobank.nl <marian.szabo@rabobank.nl>
2025-06-10 00:39:35 +0200

    Fixed wrong value for the thirty years mortgage interest rate

--- src/main/java/nl/ing/mortgage/affordability/model/MortgageInterestRate.java
+++ src/main/java/nl/ing/mortgage/affordability/model/MortgageInterestRate.java
@@ -9,7 +9,7 @@ public enum MortgageInterestRate {
 
     TEN_YEARS_MORTGAGE(10 * 12, 2.3F, LocalDateTime.of(2024, 10, 10, 0, 0, 0, 0)),
     TWENTY_YEARS_MORTGAGE(20 * 12, 2.8F, LocalDateTime.of(2025, 3, 10, 0, 0, 0, 0)),
-    THIRTY_YEARS_MORTGAGE(20 * 12, 3.5F, LocalDateTime.of(2025, 4, 10, 0, 0, 0, 0));
+    THIRTY_YEARS_MORTGAGE(30 * 12, 3.5F, LocalDateTime.of(2025, 4, 10, 0, 0, 0, 0));
 
     final int maturityPeriodInMonths;
     final float interestRate;
