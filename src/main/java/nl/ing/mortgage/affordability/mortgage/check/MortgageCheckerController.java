f438621c7362100922c57f8bb2277f465e73af63
marian.szabo@rabobank.nl <marian.szabo@rabobank.nl>
2025-06-10 11:36:45 +0200

    Called the mortgageAffordabilityService from the MortgageCheckerController to check if a mortgage is affordable

--- src/main/java/nl/ing/mortgage/affordability/mortgage/check/MortgageCheckerController.java
+++ src/main/java/nl/ing/mortgage/affordability/mortgage/check/MortgageCheckerController.java
@@ -1,5 +1,6 @@
 package nl.ing.mortgage.affordability.mortgage.check;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/mortgage-check")
 public class MortgageCheckerController {
+    private final MortgageAffordabilityService mortgageAffordabilityService;
+
+    @Autowired
+    public MortgageCheckerController(final MortgageAffordabilityService mortgageAffordabilityService) {
+        this.mortgageAffordabilityService = mortgageAffordabilityService;
+    }
 
     @PostMapping
     public MortgageAffordabilityResponse check(@RequestBody MortgageAffordabilityRequest mortgageAffordabilityRequest) {
-        return new MortgageAffordabilityResponse();
+        return new MortgageAffordabilityResponse(mortgageAffordabilityService.checkMortgageRequest(mortgageAffordabilityRequest));
     }
 }
--- src/test/java/nl/ing/mortgage/affordability/mortgage/check/MortgageCheckerControllerTest.java
+++ src/test/java/nl/ing/mortgage/affordability/mortgage/check/MortgageCheckerControllerTest.java
@@ -1,10 +1,16 @@
 package nl.ing.mortgage.affordability.mortgage.check;
 
+import nl.ing.mortgage.affordability.model.AffordabilityDecision;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -16,6 +22,9 @@ class MortgageCheckerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private MortgageAffordabilityService affordabilityService;
+
     @Test
     void urlShouldBeCorrect() throws Exception {
         var anyValidRequestAsJson = """
@@ -26,11 +35,29 @@ class MortgageCheckerControllerTest {
                     "homeValue": 150000
                 }
                 """;
-
+        when(affordabilityService.checkMortgageRequest(any())).thenReturn(AffordabilityDecision.notAffordable());
         this.mockMvc.perform(post("/api/mortgage-check")
                 .contentType(APPLICATION_JSON)
                 .content(anyValidRequestAsJson)
         ).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void affordabilityServiceShouldBeCalled() throws Exception {
+        var anyValidRequestAsJson = """
+                {
+                    "income":50000,
+                    "maturityPeriod": 20,
+                    "loanValue": 100000,
+                    "homeValue": 150000
+                }
+                """;
+        when(affordabilityService.checkMortgageRequest(any())).thenReturn(AffordabilityDecision.notAffordable());
+        this.mockMvc.perform(post("/api/mortgage-check")
+                .contentType(APPLICATION_JSON)
+                .content(anyValidRequestAsJson)
+        );
+        verify(affordabilityService, times(1)).checkMortgageRequest(any());
     }
 
 
